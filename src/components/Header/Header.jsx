c8e2bebe99a6463a17da99ae3319b72b65b01f18
Mehedi Hasan <mhjibon.info@gmail.com>
2023-08-27 22:36:07 +0600

    fetch the data and callback function done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <nav class="navbar navbar-light bg-light justify-content-between">
-                <a class="navbar-brand">Navbar</a>
-                <form class="form-inline">
+            <nav className="navbar navbar-light bg-light justify-content-between">
+                <a className="navbar-brand">Navbar</a>
+                <form className="form-inline">
                 <input
-                    class="form-control mr-sm-2"
+                    className="form-control mr-sm-2"
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleCart from '../SingleCart/SingleCart';
 
 const Home = () => {
+    const [movies ,setMovies] = useState([]);
+
+    useEffect( () => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setMovies(data));
+    }, [])
+
     return (
         <div>
             <div className="movie-container">
-                <div className='movie-card card text-center w-100 m-auto'>
-                    <div className='movie-poster w-25 m-auto'>
-                        <img className='w-75' src="https://c8.alamy.com/comp/C2WJYB/the-dark-knight-poster-display-at-the-arclight-theater-los-angeles-C2WJYB.jpg" alt="" />
-                    </div>
-                    <h3> Dark Movie</h3>
-                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing. </p>
-                    <div className="timeAndRating d-flex justify-content-around">
-                        <p> WatchTime </p>
-                        <p> Rating </p>
-                    </div>
-                    <button className='btn btn-info w-75 m-auto'> Book Now </button>
-                </div>
+                {
+                    movies.map(movie => <SingleCart
+                        key={movie.id}
+                        movie={movie}
+                    ></SingleCart>)
+                }
             </div>
         </div>
     );
